b68a2ba552fa72a5df5d51b887a953a216730d61
jesus  henriquez mura <jesus.henriquez.m@usach.cl>
2021-12-14 12:13:01 -0300

    add docker to project

--- src/invoices/invoices.controller.ts
+++ src/invoices/invoices.controller.ts
@@ -3,16 +3,27 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
   Put,
   Query,
+  Res,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBody, ApiConsumes, ApiQuery, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { jwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { InvoicesI } from 'src/common/interface/invoices.interfaces';
 import { RecordsI } from 'src/common/interface/records.interface';
 import { InvoiceDTO } from './dto/invoces.dto';
@@ -21,15 +32,29 @@ import { InvoicesService } from './invoices.service';
 
 @ApiTags('Invoices')
 @Controller('api/v1/invoices')
+@ApiBearerAuth()
+@UseGuards(jwtAuthGuard)
 export class InvoicesController {
   constructor(private readonly invoceService: InvoicesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Created new Invoice' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'invoices generated',
+    type: InvoiceDTO,
+  })
   createdInvoices(@Body() invoicesDTO: InvoiceDTO) {
-    return this.invoceService.created(invoicesDTO);
+    const invoice = this.invoceService.created(invoicesDTO);
+    return {
+      statCode: HttpStatus.OK,
+      message: 'generated',
+      data: invoice,
+    };
   }
 
   @Post('invoices/record')
+  @ApiOperation({ summary: 'Created invoices by file' })
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     schema: {
@@ -50,26 +75,36 @@ export class InvoicesController {
   }
 
   @Get('/report')
+  @ApiOperation({ summary: 'Get invoices by params' })
   async getReport(@Query() params: InvoiceReportDTO): Promise<RecordsI[]> {
     return this.invoceService.getRecord(params);
   }
 
   @Get()
-  FindAllInvoices(): Promise<InvoicesI[]> {
-    return this.invoceService.findAll();
+  @ApiOperation({ summary: 'Get all invoices' })
+  async FindAllInvoices(@Res() res): Promise<InvoicesI[]> {
+    const data = await this.invoceService.findAll();
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: data,
+    });
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get invoice by id mongo' })
   FindByID(@Param('id', ParseIntPipe) id: number): Promise<InvoicesI> {
     return this.invoceService.findById(id);
   }
 
   @Get('invoice/:id')
+  @ApiOperation({ summary: 'Get invoice by invoice id' })
   FindByIDInvoice(@Param('id', ParseIntPipe) id: number): Promise<InvoicesI> {
     return this.invoceService.findByInvoiceId(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update invoice' })
   updateInvoice(
     @Param('id', ParseIntPipe) id: number,
     @Body() invoicesDTO: InvoiceDTO,
@@ -78,6 +113,7 @@ export class InvoicesController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Deleted invoice' })
   DeletedInvoice(@Param('id', ParseIntPipe) id: number) {
     return this.invoceService.deleted(id);
   }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { RedocModule, RedocOptions } from 'nestjs-redoc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,11 +18,27 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
 
-  SwaggerModule.setup('/api/docs', app, document, {
+  const redocOptions: RedocOptions = {
+    favicon:
+      'https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2019/05/wordpress-favicon.jpg',
+    title: 'API Reservas',
+    logo: {
+      url: 'https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2019/05/wordpress-favicon.jpg',
+      backgroundColor: '#0082B7',
+    },
+    sortPropsAlphabetically: true,
+    hideDownloadButton: false,
+    hideHostname: false,
+    noAutoAuth: false,
+  };
+
+  SwaggerModule.setup('/api/swagger', app, document, {
     swaggerOptions: {
       filter: true,
     },
   });
+
+  await RedocModule.setup('/api/docs', app, document, redocOptions);
   await app.listen(process.env.PORT || 3000);
 }
 
